6182cfe420fb9a466c59ac7e95ddc74935a83a50
agray5 <grayaprild@gmail.com>
2019-08-22 17:11:09 -0500

    wasd controls

--- app/scripts/objects/input.js
+++ app/scripts/objects/input.js
@@ -1,11 +1,19 @@
 
 import Player from './player';
 
+
 export default class Input {
 /**
  * @param {Phaser.Scene} scene 
  */
   constructor (scene, player) {
+    this.WASD = scene.input.keyboard.addKeys(
+      {
+        up:   Phaser.Input.Keyboard.KeyCodes.W,
+        down: Phaser.Input.Keyboard.KeyCodes.S,
+        left: Phaser.Input.Keyboard.KeyCodes.A,
+        right:Phaser.Input.Keyboard.KeyCodes.D
+      });
     this.input = scene.input.keyboard.createCursorKeys();
     this.scene = scene;
   }
@@ -14,14 +22,14 @@ export default class Input {
     /** @type{Player} */
     const player = scene.get("player");
 
-    if (this.input.left.isDown) {
+    if (this.input.left.isDown || this.WASD.left.isDown) {
       player.walk(-200, true);
       /*
       player.body.setVelocityX(-200); // move left
       if(player.body.onFloor()) player.state = 'walk'; // play walk animation
       player.flipX = true; // flip the sprite to the left*/
     }
-    else if (this.input.right.isDown) {
+    else if (this.input.right.isDown || this.WASD.right.isDown) {
       player.walk(200, false);
       /*
       player.body.setVelocityX(200); // move right
@@ -35,7 +43,7 @@ export default class Input {
       player.body.setVelocityX(0);
       if(player.body.onFloor()) player.state = 'idle';*/
     }  
-    if ((this.input.space.isDown || this.input.up.isDown) && player.body.onFloor()) {
+    if ((this.input.space.isDown || this.input.up.isDown || this.WASD.up.isDown) && player.body.onFloor()) {
       player.jump(-500);
       /*
       player.state = 'jump';
